71f2d2e711d42ac280db2dd98e2fd4a04172a22c
José Ángel Durán Cerviño <jdcervinho@esei.uvigo.es>
2019-10-01 09:36:40 +0200

    Adds base code for Executor Thread

---
+++ ExecutorThread.java
@@ -0,0 +1,40 @@
+
+import java.net.Socket;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Map;
+
+
+public class ExecutorThread implements Runnable{
+	Socket clientSocket;
+	public ExecutorThread(Socket clientSocket){
+		this.clientSocket = clientSocket;
+	}
+	public void run(){
+		try(Socket clientSocket = this.clientSocket){
+			BufferedReader reader = new BufferedReader
+								(new InputStreamReader(clientSocket.getInputStream()));
+				DataOutputStream sender = new DataOutputStream(clientSocket);
+				String uuid;
+				uuid= reader.readLine();
+				/*Suponemos que pseudoBD es el Map<String,String> que emula a la BD y que está declarado de forma global*/
+				if(pseudoBD.contains(uuid)){
+					File file= new File(psudoBD.get(uuid));
+					if(file.isFile() && file.canRead()){
+						try(FileInputStream fileReader = new FileInputStream(fileName)){
+							sender.writeLong(file.length());
+							int aux;
+							while((aux=fileReader.read()) != -1){
+								sender.write(aux);
+							}
+						}
+					}
+				}	
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+}
