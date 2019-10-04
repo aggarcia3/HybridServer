33ffe9b45a218908cdb41acc6ea552ee1c9a7fc8
aggarcia3 <27727839+aggarcia3@users.noreply.github.com>
2019-10-04 22:52:42 +0200

    Launcher: Añadir código básico
    Simplemente se encarga de crear un HybridServer, iniciarlo y detenerlo a
    petición del usuario, de modo similar a los tests de la primera semana.
    Este cambio permite que el servidor sea utilizable desde la línea de
    comandos, fuera de los tests.

--- HybridServer/src/es/uvigo/esei/dai/hybridserver/Launcher.java
+++ HybridServer/src/es/uvigo/esei/dai/hybridserver/Launcher.java
@@ -1,7 +1,20 @@
 package es.uvigo.esei.dai.hybridserver;
 
+import java.io.IOException;
+
 public class Launcher {
+	// TODO
 	public static void main(String[] args) {
-		
+		final HybridServer server = new HybridServer();
+
+		System.out.println("-- " + HybridServer.NAME.toUpperCase() + " --");
+		server.start();
+
+		System.out.println("Press Enter to shutdown the server.");
+		try {
+			System.in.read();
+		} catch (final IOException exc) {}
+
+		server.stop();
 	}
 }
