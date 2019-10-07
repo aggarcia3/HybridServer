20dc141766a822ff5f32c2f987d670d15d09d2ec
aggarcia3 <27727839+aggarcia3@users.noreply.github.com>
2019-10-07 22:16:37 +0200

    ResourceReader: Eliminar patrón singleton. Mantener caché de recursos
    Esto mejora el rendimiento y permite un uso mucho más flexible y
    sencillo de la clase, sin que los usuarios tengan que preocuparse de los
    intríngulis de los cargadores de clases.
    
    Por otro lado, ahora los usuarios de esta clase también podrán comprobar
    si la carga del recurso tuvo éxito o no, y actuar en consecuencia (útil
    para devolver un estado 500, por ejemplo).
    
    Esta confirmación depende de cambios que se publicarán en futuras
    confirmaciones para funcionar.

--- HybridServer/src/es/uvigo/esei/dai/hybridserver/ResourceReader.java
+++ HybridServer/src/es/uvigo/esei/dai/hybridserver/ResourceReader.java
@@ -8,8 +8,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
-
-import static es.uvigo.esei.dai.hybridserver.HybridServer.LOGGER;
+import java.util.logging.Logger;
 
 /**
  * Contains logic to read resource files of the application. Resource files are
@@ -24,43 +23,37 @@ final class ResourceReader {
 	private static final int TRANSFER_BUFFER_SIZE = 4 * 1024; // 4 KiB
 
 	private final Map<String,String> loadedTextResources = new ConcurrentHashMap<>(2); // 2 is the number of resources
+	private final HybridServer server;
 
-	private ResourceReader() {}
-
-	// Initialization-on-demand holder idiom
-	private static final class ResourceReaderInstanceHolder {
-		static final ResourceReader INSTANCE = new ResourceReader();
+	/**
+	 * Creates a new resource reader for a server.
+	 *
+	 * @param server The server whose class loader is responsible for the resource
+	 *               loading.
+	 */
+	public ResourceReader(final HybridServer server) {
+		this.server = server;
 	}
 
 	/**
-	 * Gets the only instance that is allowed to exist of this object in the JVM.
+	 * Reads a text resource bundled with the server to a string.
 	 *
-	 * @return The instance of this object.
+	 * @param name The name of the resource.
+	 * @return The resource, as a string. It can be null if an error occurred during
+	 *         the operation; if this is the case, users are not encouraged to retry
+	 *         the operation until it returns a different value, because it won't.
 	 */
-	public static ResourceReader get() {
-		return ResourceReaderInstanceHolder.INSTANCE;
-	}
+	public String readTextResourceToString(final String name) {
+		final Logger logger = server.getLogger();
 
-	/**
-	 * Reads a text resource to a string.
-	 *
-	 * @param requestedByClass The class whose classloader should know how to find
-	 *                         the resource.
-	 * @param name             The name of the resource.
-	 * @return The resource, as a string. It can be null if an error occured during
-	 *         the operation.
-	 */
-	public String readTextResourceToString(final Class<?> requestedByClass, final String name) {
-		final Object[] logParameters = new Object[] { name, requestedByClass.getSimpleName() };
-
-		LOGGER.log(Level.FINE, "Trying to serve resource {0} for {1} from the cache", logParameters);
+		logger.log(Level.FINE, "Trying to serve resource {0} from the cache", name);
 
 		return loadedTextResources.computeIfAbsent(name, (String resource) -> {
-			LOGGER.log(Level.FINE, "Resource not in cache. Reeading resource {0} for {1} from storage", logParameters);
+			logger.log(Level.FINE, "Resource not in cache. Reeading resource {0} from storage", name);
 
 			// Read the resource from the input stream
 			long readStart = System.currentTimeMillis();
-			try (final Reader r = new InputStreamReader(requestedByClass.getResourceAsStream(name), StandardCharsets.UTF_8)) {
+			try (final Reader r = new InputStreamReader(server.getClass().getResourceAsStream(name), StandardCharsets.UTF_8)) {
 				// Create a writer backed by a string buffer
 				try (final Writer w = new StringWriter(WRITER_BUFFER_SIZE)) {
 					final char[] buf = new char[TRANSFER_BUFFER_SIZE];
@@ -72,10 +65,10 @@ final class ResourceReader {
 					}
 
 					final String toret = w.toString();
-					LOGGER.log(Level.FINE,
-						"Done reading resource {0} requested by {1} ({2} ms, {3} characters)",
+					logger.log(Level.FINE,
+						"Done reading resource {0} ({1} ms, {2} characters)",
 						new Object[] {
-							name, requestedByClass.getSimpleName(),
+							name,
 							System.currentTimeMillis() - readStart, toret.length()
 						}
 					);
@@ -83,7 +76,7 @@ final class ResourceReader {
 					return toret;
 				}
 			} catch (final Exception exc) {
-				LOGGER.log(Level.SEVERE, "Couldn't get the resource {0} requested by {1}. Returning null string", logParameters);
+				logger.log(Level.SEVERE, "Couldn't get the resource {0}. Returning null string", name);
 				return null;
 			}
 		});
