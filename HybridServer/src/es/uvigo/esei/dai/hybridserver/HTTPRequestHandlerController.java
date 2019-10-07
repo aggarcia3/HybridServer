bb853a7a58030a6cee9368c88bdb44398c27d79a
aggarcia3 <27727839+aggarcia3@users.noreply.github.com>
2019-10-07 22:36:48 +0200

    HTTPRequestHandlerController: Refactorizar y corregir cierre de sockets
    Esta confirmación permite que el controlador funcione con cambios
    introducidos en confirmaciones anteriores recientes. También arregla una
    situación en la que, al ocurrir un error al entender la petición, el
    servidor no enviaba una respuesta al cliente, producida por cerrar
    primero el socket de entrada antes que el de salida.

--- HybridServer/src/es/uvigo/esei/dai/hybridserver/HTTPRequestHandlerController.java
+++ HybridServer/src/es/uvigo/esei/dai/hybridserver/HTTPRequestHandlerController.java
@@ -9,6 +9,7 @@ import java.io.Reader;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import es.uvigo.esei.dai.hybridserver.http.HTTPHeaders;
 import es.uvigo.esei.dai.hybridserver.http.HTTPMissingHeaderException;
@@ -19,8 +20,6 @@ import es.uvigo.esei.dai.hybridserver.http.HTTPResponseStatus;
 import es.uvigo.esei.dai.hybridserver.http.HTTPUnsupportedContentEncodingException;
 import es.uvigo.esei.dai.hybridserver.http.HTTPUnsupportedHeaderException;
 
-import static es.uvigo.esei.dai.hybridserver.HybridServer.LOGGER;
-
 /**
  * Orchestrates operations to implement the HTTP request handling business
  * logic.
@@ -28,39 +27,44 @@ import static es.uvigo.esei.dai.hybridserver.HybridServer.LOGGER;
  * @author Alejandro González García
  */
 final class HTTPRequestHandlerController {
-	private final static String STATUS_HTML = ResourceReader.get().readTextResourceToString(HTTPRequestHandlerController.class, "/es/uvigo/esei/dai/hybridserver/resources/status_code.htm");
 	private final static String IO_EXCEPTION_MSG = "An I/O error has occured while handling a request";
 
+	private final HybridServer server;
 	private final Reader input;
 	private final Writer output;
 	private final HTTPHeaders version;
+	private final String statusHtml;
 	private boolean handledRequest = false;
 
 	/**
 	 * Instantiates a new request handler controller.
 	 *
+	 * @param server The server for which this controller will handle requests.
 	 * @param input  The input stream to associate this controller with.
 	 * @param output The output stream to associate this controller with.
 	 * @param version The version of the HTTP response to send.
 	 */
-	public HTTPRequestHandlerController(final InputStream input, final OutputStream output, final HTTPHeaders version) {
-		if (input == null || output == null || version == null) {
-			throw new IllegalArgumentException("A request handler must be constructed with non-null streams and HTTP version");
+	public HTTPRequestHandlerController(final HybridServer server, final InputStream input, final OutputStream output, final HTTPHeaders version) {
+		if (server == null || input == null || output == null || version == null) {
+			throw new IllegalArgumentException("A request handler must be constructed with non-null streams, server and HTTP version");
 		}
 
+		this.server = server;
 		this.input = new InputStreamReader(input, StandardCharsets.UTF_8);
 		this.output = new OutputStreamWriter(output, StandardCharsets.UTF_8);
 		this.version = version;
+		this.statusHtml = server.getResourceReader().readTextResourceToString("/es/uvigo/esei/dai/hybridserver/resources/status_code.htm");
 	}
 
 	/**
 	 * Instantiates a new request handler controller.
 	 *
+	 * @param server The server for which this controller will handle requests.
 	 * @param input  The input stream to associate this controller with.
 	 * @param output The output stream to associate this controller with.
 	 */
-	public HTTPRequestHandlerController(final InputStream input, final OutputStream output) {
-		this(input, output, HTTPHeaders.HTTP_1_1);
+	public HTTPRequestHandlerController(final HybridServer server, final InputStream input, final OutputStream output) {
+		this(server, input, output, HTTPHeaders.HTTP_1_1);
 	}
 
 	/**
@@ -72,18 +76,20 @@ final class HTTPRequestHandlerController {
 	 * after calling this method, as this method closes them automatically.
 	 */
 	public void handleIncoming() {
+		final Logger logger = server.getLogger();
+
 		if (!handledRequest) {
 			try {
-				LOGGER.log(Level.FINE, "Handling incoming request");
+				logger.log(Level.FINE, "Handling incoming request");
 				final long handlingStart = System.currentTimeMillis();
 
 				// Read a syntactically-valid HTTP request from the client, handle it to get its
 				// response, and send the response
-				final HTTPRequest request = new HTTPRequest(input);
+				final HTTPRequest request = new HTTPRequest(server, input);
 				final HTTPRequestHandler handler = HTTPRequestHandlerFactory.handlerFor(request);
 				final HTTPResponse response = handler.handle();
 
-				LOGGER.log(Level.FINE,
+				logger.log(Level.FINE,
 					"Generated response {0} for incoming {1} request to {2} in {3} ms, using {4}",
 					new Object[] {
 						response.getStatus(),
@@ -95,16 +101,6 @@ final class HTTPRequestHandlerController {
 				);
 
 				response.print(output);
-
-				// Close the readers and writers and the streams
-				// who are being decorated. We ignore errors because
-				// it's not important we failed doing this (when closing
-				// the output stream, we might as well close the input one,
-				// as they will likely be related to the same socket)
-				try {
-					output.close();
-					input.close();
-				} catch (final IOException ignore) {}
 			} catch (final HTTPParseException exc) {
 				final Throwable cause = exc.getCause();
 
@@ -123,28 +119,38 @@ final class HTTPRequestHandlerController {
 				) {
 					status = HTTPResponseStatus.S411;
 				} else {
-					LOGGER.log(Level.INFO, "The client has sent a bad request", exc);
+					logger.log(Level.INFO, "The client has sent a bad request", exc);
 				}
 
 				try {
 					respondStatus(status);
 				} catch (final IOException exc2) {
-					LOGGER.log(Level.WARNING, IO_EXCEPTION_MSG, exc);
+					logger.log(Level.WARNING, IO_EXCEPTION_MSG, exc);
 				}
 			} catch (final IOException exc) {
-				LOGGER.log(Level.WARNING, IO_EXCEPTION_MSG, exc);
-			} finally {
-				// Close the reader and writer.
-				// This way ensures the writer actually writes to the stream
+				logger.log(Level.WARNING, IO_EXCEPTION_MSG, exc);
+			} catch (final Exception exc) {
+				logger.log(Level.WARNING, "A handler throwed an exception, and that shouldn't happen by contract. Please fix the handler!", exc);
 				try {
-					input.close();
+					respondStatus(HTTPResponseStatus.S500);
+				} catch (final IOException exc2) {
+					logger.log(Level.WARNING, IO_EXCEPTION_MSG, exc);
+				}
+			} finally {
+				// Close the readers and writers and the streams
+				// who are being decorated. We ignore errors because
+				// it's not important we failed doing this (when closing
+				// the output stream, we might as well close the input one,
+				// as they will likely be related to the same socket)
+				try {
 					output.close();
+					input.close();
 				} catch (final IOException ignored) {}
 
 				handledRequest = true;
 			}
 		} else {
-			LOGGER.log(Level.FINE, "Request already handled by this controller, ignoring");
+			logger.log(Level.FINE, "Request already handled by this controller, ignoring");
 		}
 	}
 
@@ -155,17 +161,17 @@ final class HTTPRequestHandlerController {
 	 * @throws IOException If some I/O error occurs during the operation.
 	 */
 	private void respondStatus(final HTTPResponseStatus status) throws IOException {
-		LOGGER.log(Level.FINE, "Responding to the client with {0}", status.getCode());
+		server.getLogger().log(Level.FINE, "Responding to the client with {0}", status.getCode());
 
 		final HTTPResponse response = new HTTPResponse()
 			.setStatus(status)
 			.setVersion(version.getHeader());
 
-		if (STATUS_HTML != null) {
+		if (statusHtml != null) {
 			response.putParameter(HTTPHeaders.CONTENT_TYPE.getHeader(), "text/html; charset=UTF-8")
-				.setContent(String.format(STATUS_HTML, status.getCode(), status.getStatus()));
+				.setContent(String.format(statusHtml, status.getCode(), status.getStatus()));
 		} else {
-			LOGGER.log(Level.WARNING, "Couldn't get the HTML status page resource, so no message body was sent for the error");
+			server.getLogger().log(Level.WARNING, "Couldn't get the HTML status page resource, so no message body was sent for the error");
 		}
 
 		response.print(output);
