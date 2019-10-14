64a66f7a19d50ada5652aec452e569b81d17906e
aggarcia3 <27727839+aggarcia3@users.noreply.github.com>
2019-10-14 19:21:57 +0200

    Launcher: Añadir lógica para leer el fichero de configuración especificado en línea de comandos

--- HybridServer/src/es/uvigo/esei/dai/hybridserver/Launcher.java
+++ HybridServer/src/es/uvigo/esei/dai/hybridserver/Launcher.java
@@ -1,46 +1,68 @@
 package es.uvigo.esei.dai.hybridserver;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
 
+/**
+ * Launches Hybrid Server, loading its configuration parameters.
+ *
+ * @author Alejandro González García
+ */
 public class Launcher {
-	// TODO
-	public static void main(String[] args) {
-		// Page array and map creating code extracted from
-		// ClientRequetsTest.java (sic)
+	/**
+	 * Entry point of the application.
+	 *
+	 * @param args The command line parameters received from the execution
+	 *             environment.
+	 */
+	public static void main(final String[] args) {
+		final Properties configuration = new Properties();
+		HybridServer server;
 
-		// El servidor debe tener las siguientes páginas en memoria
-		String[][] pages = new String[][] {
-		//  { "uuid",                                 "texto contenido por la página"                               }
-		    { "6df1047e-cf19-4a83-8cf3-38f5e53f7725", "This is the html page 6df1047e-cf19-4a83-8cf3-38f5e53f7725." },
-		    { "79e01232-5ea4-41c8-9331-1c1880a1d3c2", "This is the html page 79e01232-5ea4-41c8-9331-1c1880a1d3c2." },
-		    { "a35b6c5e-22d6-4707-98b4-462482e26c9e", "This is the html page a35b6c5e-22d6-4707-98b4-462482e26c9e." },
-		    { "3aff2f9c-0c7f-4630-99ad-27a0cf1af137", "This is the html page 3aff2f9c-0c7f-4630-99ad-27a0cf1af137." },
-		    { "77ec1d68-84e1-40f4-be8e-066e02f4e373", "This is the html page 77ec1d68-84e1-40f4-be8e-066e02f4e373." },
-		    { "8f824126-0bd1-4074-b88e-c0b59d3e67a3", "This is the html page 8f824126-0bd1-4074-b88e-c0b59d3e67a3." },
-		    { "c6c80c75-b335-4f68-b7a7-59434413ce6c", "This is the html page c6c80c75-b335-4f68-b7a7-59434413ce6c." },
-		    { "f959ecb3-6382-4ae5-9325-8fcbc068e446", "This is the html page f959ecb3-6382-4ae5-9325-8fcbc068e446." },
-		    { "2471caa8-e8df-44d6-94f2-7752a74f6819", "This is the html page 2471caa8-e8df-44d6-94f2-7752a74f6819." },
-		    { "fa0979ca-2734-41f7-84c5-e40e0886e408", "This is the html page fa0979ca-2734-41f7-84c5-e40e0886e408." }
-		};
+		if (args.length == 1) {
+			try (final Reader configurationReader = new InputStreamReader(new FileInputStream(args[1]), StandardCharsets.UTF_8)) {
+				// Load the configuration properties from the reader
+				configuration.load(configurationReader);
 
-		// Creación del servidor con las páginas ya en memoria
-		final Map<String, String> pagesMap = new HashMap<>();
-		for (String[] page : pages) {
-			pagesMap.put(page[0], page[1]);
+				// Initialize the Hybrid Server with the configuration
+				server = new HybridServer(configuration);
+
+				// Start it
+				server.start();
+
+				// Wait for Enter to stop the server
+				System.out.println("Press Enter to shutdown the server.");
+				try {
+					System.in.read();
+				} catch (final IOException exc) {
+					throw new AssertionError("I/O error while reading from standard input. This shouldn't happen");
+				}
+
+				server.stop();
+			} catch (final FileNotFoundException exc) {
+				printErrorMessageAndExit("Unable to open the file " + args[1] + " for reading. Does it exist, and is it a file?");
+			} catch (final IOException exc) {
+				printErrorMessageAndExit("An I/O error has occured while reading the configuration file");
+			}
+		} else {
+			printErrorMessageAndExit("Unexpected number of arguments supplied to the launcher");
 		}
+	}
 
-		final HybridServer server = new HybridServer(pagesMap);
-
-		System.out.println("-- " + server.getName().toUpperCase() + " --");
-		server.start();
-
-		System.out.println("Press Enter to shutdown the server.");
-		try {
-			System.in.read();
-		} catch (final IOException exc) {}
-
-		server.stop();
+	/**
+	 * Prints an error message to System.err, and then exits with a nonzero status
+	 * code (which signals error).
+	 *
+	 * @param message The message to show in System.err.
+	 */
+	private static void printErrorMessageAndExit(final String message) {
+		System.err.println(message);
+		System.err.println("Syntax: " + Launcher.class.getSimpleName() + " (configuration file)");
+		System.exit(1);
 	}
 }
