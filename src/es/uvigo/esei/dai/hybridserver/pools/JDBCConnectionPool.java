2f74695875a7089d39ea8cd8bfc25f2ac453a65f
aggarcia3 <27727839+aggarcia3@users.noreply.github.com>
2019-12-09 14:33:43 +0100

    WIP 2: pool de conexiones común a todos los DAO

--- src/es/uvigo/esei/dai/hybridserver/pools/JDBCConnectionPool.java
+++ src/es/uvigo/esei/dai/hybridserver/pools/JDBCConnectionPool.java
@@ -1,13 +1,17 @@
 package es.uvigo.esei.dai.hybridserver.pools;
 
+import java.io.Closeable;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
-public final class JDBCConnectionPool {
+public final class JDBCConnectionPool implements Closeable {
 	private final Map<Integer, Connection> dbConnections = new ConcurrentHashMap<>();
 	private final ThreadLocal<Integer> currentThreadId;
 	private final AtomicInteger nextThreadId;
@@ -34,7 +38,7 @@ public final class JDBCConnectionPool {
 		return JDBCConnectionPoolInstanceHolder.INSTANCE;
 	}
 
-	public final Connection getConnectionForThread(final Supplier<Connection> connectionStrategy, final int timeoutSeconds) throws SQLException {
+	public final Connection getConnectionForCurrentThread(final Supplier<Connection> connectionStrategy, final int timeoutSeconds) throws SQLException {
 		final int currentThreadId = this.currentThreadId.get();
 
 		if (connectionStrategy == null) {
@@ -60,5 +64,74 @@ public final class JDBCConnectionPool {
 		}
 
 		return currentThreadConnection;
+	}
+
+	public final void closeForCurrentThread() throws SQLException {
+		final int currentThreadId = this.currentThreadId.get();
+		final Connection threadConnection = dbConnections.get(currentThreadId);
+
+		if (threadConnection != null) {
+			// Bail out if the connection is closed. If an exception occurs, assume
+			// it is not closed
+			try {
+				if (threadConnection.isClosed()) {
+					return;
+				}
+			} catch (final SQLException ignored) {}
+
+			threadConnection.close();
+			// If we didn't close it successfully, it won't be removed from the map
+			dbConnections.remove(currentThreadId);
+		}
+	}
+
+	@Override
+	public final void close() throws IOException {
+		SQLException firstException = null;
+
+		final Iterator<Connection> iter = dbConnections.values().iterator();
+		while (iter.hasNext()) {
+			final Connection connection = iter.next();
+
+			try {
+				closeConnection(connection);
+				iter.remove(); // Removes the entry on the map
+			} catch (final SQLException exc) {
+				if (firstException != null) {
+					firstException.setNextException(exc);
+				} else {
+					firstException = exc;
+				}
+			}
+		}
+
+		// Delay throwing exceptions until a best effort to close all the connections
+		// has been made
+		if (firstException != null) {
+			throw new IOException(firstException);
+		}
+	}
+
+	private final void closeConnection(final Connection connection) throws SQLException {
+		SQLException throwedException = null;
+
+		try {
+			if (!connection.isClosed() && !connection.getAutoCommit()) {
+				connection.rollback();
+			}
+		} catch (final SQLException exc) {
+			throwedException = exc;
+		} finally {
+			// Close the connection no matter if we succeeded doing the rollback
+			try {
+				connection.close();
+			} catch (final SQLException exc) {
+				throwedException = exc;
+			}
+		}
+
+		if (throwedException != null) {
+			throw throwedException;
+		}
 	}
 }
